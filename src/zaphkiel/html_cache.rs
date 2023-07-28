5a7d974703e568efd56a8d4259d81f20669ecada
pawarherschel <pawarherschel@gmail.com>
2023-07-28 17:55:44 +0530

    check

--- src/zaphkiel/html_cache.rs
+++ src/zaphkiel/html_cache.rs
@@ -1,6 +1,9 @@
 use reqwest::Url;
 use scraper::Html;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs;
+use std::ops::Deref;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -11,7 +14,7 @@ pub struct HtmlCache {
     pub stats: Arc<RwLock<HtmlCacheStats>>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq, Hash, Clone)]
 #[allow(dead_code)]
 pub struct HtmlCacheStats {
     pub cache_hits: u64,
@@ -20,6 +23,27 @@ pub struct HtmlCacheStats {
 
 impl HtmlCache {
     pub fn new() -> Self {
+        match (
+            fs::read_to_string("cache.json"),
+            fs::read_to_string("stats.json"),
+        ) {
+            (Ok(cache), Ok(stats)) => {
+                let cache: HashMap<String, String> = serde_json::from_str(&cache).unwrap();
+                let stats: HtmlCacheStats = serde_json::from_str(&stats).unwrap();
+
+                let cache: HashMap<Url, Html> = cache
+                    .into_iter()
+                    .map(|(k, v)| (Url::parse(&k).unwrap(), Html::parse_document(&v)))
+                    .collect();
+
+                Self {
+                    cache: Arc::new(RwLock::new(cache)),
+                    stats: Arc::new(RwLock::new(stats)),
+                }
+            }
+            _ => Self::default(),
+        }
+
         Self::default()
     }
 }
@@ -46,5 +70,38 @@ impl HtmlCache {
     }
     pub async fn miss(&self) {
         self.stats.write().await.cache_misses += 1;
+    }
+
+    pub async fn pump(&self) {
+        let cache = fs::read_to_string("cache.json").unwrap();
+        let stats = fs::read_to_string("stats.json").unwrap();
+
+        let cache: HashMap<String, String> = serde_json::from_str(&cache).unwrap();
+        let stats: HtmlCacheStats = serde_json::from_str(&stats).unwrap();
+
+        let cache: HashMap<Url, Html> = cache
+            .into_iter()
+            .map(|(k, v)| (Url::parse(&k).unwrap(), Html::parse_document(&v)))
+            .collect();
+
+        *self.cache.write().await = cache;
+        *self.stats.write().await = stats;
+    }
+    pub async fn dump(&self) {
+        let serialize_friendly_map: HashMap<String, String> = {
+            let cache = self.cache.read().await.deref().clone();
+            cache
+                .into_iter()
+                .map(|(k, v)| (k.to_string(), v.html()))
+                .collect()
+        };
+
+        let stats = self.stats.read().await.deref().clone();
+
+        let cache = serde_json::to_string(&serialize_friendly_map).unwrap();
+        let stats = serde_json::to_string(&stats).unwrap();
+
+        fs::write("cache.json", cache).unwrap();
+        fs::write("stats.json", stats).unwrap();
     }
 }
