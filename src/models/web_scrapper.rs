7eb5cb1c1fbc3e410105802908bf9ea07e4ac438
pawarherschel <pawarherschel@gmail.com>
2023-07-29 18:18:23 +0530

    migration completed

--- src/models/web_scrapper.rs
+++ src/models/web_scrapper.rs
@@ -1,17 +1,17 @@
 use std::cell::RefCell;
+use std::fs;
 
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::blocking::Client;
+use ureq::{Agent, AgentBuilder};
 
 use crate::zaphkiel::cache::{Cache, HtmlCacheStats};
 
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct WebScraper {
-    client: Client,
+    client: Agent,
     cache: RefCell<Cache>,
     cookie: String,
-    user_agent: String,
 }
 
 impl WebScraper {
@@ -22,28 +22,27 @@ impl WebScraper {
 
 impl WebScraper {
     pub fn new(cookie: String, adult: bool) -> Self {
-        let client = Client::builder()
-            .cookie_store(true)
-            .build()
-            .expect("failed to build client");
+        let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/108.0.0.0 \
+            Safari/537.36";
+
+        let client = AgentBuilder::new().user_agent(user_agent).build();
 
         let session_cookie = format!("_plaza_session_nktz7u={}; Secure", cookie);
         let adult_cookie = format!("adult={}; Secure", if adult { "t" } else { "f" });
         let cookie = format!("{}; {}", session_cookie, adult_cookie);
 
-        let cache = RefCell::new(Cache::new());
-
-        let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-            AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/108.0.0.0 \
-            Safari/537.36"
-            .to_string();
+        let cache = if fs::metadata("cache.json").is_ok() {
+            RefCell::new(Cache::new_from_file("cache.json".into()))
+        } else {
+            RefCell::new(Cache::new())
+        };
 
         Self {
             client,
             cookie,
             cache,
-            user_agent,
         }
     }
 }
@@ -64,11 +63,10 @@ impl WebScraper {
 
         let res = self
             .client
-            .get(url.clone())
-            .header("Cookie", self.cookie.clone())
-            .header("User-Agent", self.user_agent.clone())
-            .send()?
-            .text()?;
+            .get(&url)
+            .set("Cookie", &self.cookie.clone())
+            .call()?
+            .into_string()?;
 
         self.cache.borrow_mut().add(url, res.clone());
 
