b93723bfd0d0b25e10369bcb03b06981e376fa2f
pawarherschel <pawarherschel@gmail.com>
2023-08-02 12:29:19 +0530

    almost completed From<ItemApiResponse> trait for ItemMetadata

--- src/models/item_metadata.rs
+++ src/models/item_metadata.rs
@@ -1,5 +1,7 @@
+use crate::api_structs::items::ItemApiResponse;
+
 #[derive(Debug, Default, Clone)]
-pub struct Item {
+pub struct ItemMetadata {
     pub item: ItemInfo,
     pub author: NameWithUrl,
     pub hearts: u32,
@@ -7,6 +9,103 @@ pub struct Item {
     pub category: CategoryInfo,
     pub images: Vec<NameWithUrl>,
     pub downloads: Vec<DownloadInfo>,
+}
+
+impl ItemMetadata {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl From<ItemApiResponse> for ItemMetadata {
+    fn from(value: ItemApiResponse) -> Self {
+        let item = value.name.trim();
+        let author = value.shop;
+        let hearts = value.wish_lists_count;
+        let price = value.price;
+        let category = value.category;
+        let images = value.images;
+        let variations = value.variations;
+
+        let item = {
+            let id = u32::try_from(value.id).unwrap_or_else(|e| {
+                panic!(
+                    "unable to convert id ({}) into u32 because of error {}",
+                    value.id, e
+                )
+            });
+            let name = item.to_owned().into();
+            let url = value.url;
+            ItemInfo {
+                name: NameWithUrl { name, url },
+                id,
+            }
+        };
+        let author = {
+            let name = author.name.into();
+            let url = author.url;
+            NameWithUrl { name, url }
+        };
+        let hearts = u32::try_from(hearts).unwrap_or_else(|e| {
+            panic!(
+                "unable to convert hearts ({}) into u32 because of error {}",
+                hearts, e
+            )
+        });
+        let price = {
+            let temp = price.split(' ').collect::<Vec<_>>();
+            if temp.len() != 2 {
+                panic!("price ({}) is not in the correct format", price);
+            }
+            let number = temp[0].replace(',', "").parse::<f64>().unwrap_or_else(|e| {
+                panic!(
+                    "unable to convert price ({}) into f32 because of error {}",
+                    temp[0], e
+                )
+            });
+            let unit = temp[1].to_owned();
+            NumberWithUnit { number, unit }
+        };
+        let category = {
+            let subcategory = category.name;
+            let subcategory_url = category.url;
+            let category = category.parent.name;
+            let category_url = category.parent.url;
+
+            let category = NameWithUrl {
+                name: category.into(),
+                url: category_url,
+            };
+            let subcategory = NameWithUrl {
+                name: subcategory.into(),
+                url: subcategory_url,
+            };
+            CategoryInfo {
+                category,
+                subcategory,
+            }
+        };
+        let images = {
+            images
+                .into_iter()
+                .map(|image| {
+                    let name = image.caption.unwrap_or_default().into();
+                    let url = image.original;
+                    NameWithUrl { name, url }
+                })
+                .collect::<Vec<_>>()
+        };
+        let mut downloads = vec![];
+        ItemMetadata {
+            item,
+            author,
+            hearts,
+            price,
+            category,
+            images,
+            downloads,
+        }
+    }
 }
 
 #[derive(Debug, Default, Clone)]
@@ -18,7 +117,7 @@ pub struct ItemInfo {
 #[derive(Debug, Default, Clone)]
 pub struct CategoryInfo {
     pub category: NameWithUrl,
-    pub subcategory: Option<NameWithUrl>,
+    pub subcategory: NameWithUrl,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -36,10 +135,25 @@ pub struct NameWithTranslation {
     pub name_translated: String,
 }
 
+impl Into<NameWithTranslation> for String {
+    fn into(self) -> NameWithTranslation {
+        NameWithTranslation {
+            name: self.clone(),
+            name_translated: self,
+        }
+    }
+}
+
 #[derive(Debug, Default, Clone)]
 pub struct NameWithUrl {
     pub name: NameWithTranslation,
     pub url: String,
+}
+
+impl NameWithUrl {
+    pub fn new(name: NameWithTranslation, url: String) -> Self {
+        Self { name, url }
+    }
 }
 
 #[derive(Debug, Default, Clone)]
@@ -52,4 +166,10 @@ pub struct Badges {
 pub struct NumberWithUnit {
     pub number: f64,
     pub unit: String,
+}
+
+impl NumberWithUnit {
+    pub fn new(number: f64, unit: String) -> Self {
+        Self { number, unit }
+    }
 }
