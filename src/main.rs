263ed35ef4e34a8458bd3f9420e6e5ab171cdbd9
pawarherschel <pawarherschel@gmail.com>
2023-09-17 02:25:31 +0530

    collecting more information....... and showing it..........

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use fs::File;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 use std::time::Instant;
 
 use indicatif::ParallelProgressIterator;
@@ -46,14 +46,65 @@ fn main() {
 
     let cache = Arc::new(RwLock::new(Cache::new_with_path(path_to_cache)));
 
+    let client_get_one_errs = Arc::new(Mutex::new(vec![]));
+    let serde_json_errs = Arc::new(Mutex::new(vec![]));
+
     let all_items = time_it!(at once | "Extracting items" => all_item_numbers
         // .iter()
         .par_iter()
         .progress_with(get_pb(all_item_numbers.len() as u64, "Extracting Items"))
         .map(|id| format!("https://booth.pm/en/items/{}.json", id))
-        .filter_map(|url| CLIENT.get_one(url, Some(cache.clone())).ok())
-        .filter_map(|item| serde_json::from_str::<ItemApiResponse>(&item).ok())
+        .filter_map(|url| {
+            match CLIENT.get_one(url, Some(cache.clone())) {
+                Ok(item) => Some(item),
+                Err(err) => {
+                    client_get_one_errs.clone().lock().unwrap().push(err);
+                    None
+                }
+            }
+        })
+        .filter_map(|item| {
+            match serde_json::from_str::<ItemApiResponse>(&item) {
+                Ok(item) => Some(item),
+                Err(err) => {
+                    serde_json_errs.clone().lock().unwrap().push(err);
+                    None
+                }
+            }
+        })
         .collect::<Vec<ItemApiResponse>>()
+    );
+
+    if !client_get_one_errs.lock().unwrap().is_empty() {
+        println!("CLIENT.get_one errs: {:#?}", client_get_one_errs);
+        time_it!(at once | "writing web client errors to ron file" => {
+            let output_path = Path::new("temp/client_get_one_errs.ron");
+            let output_array = client_get_one_errs.clone().lock().unwrap().iter().map(|err| err.to_string()).collect::<Vec<_>>();
+
+            let mut file = File::create(output_path).unwrap();
+            let client_get_one_errs_pretty = ron::ser::to_string_pretty(&output_array, Default::default()).unwrap();
+            file.write_all(client_get_one_errs_pretty.as_bytes()).unwrap();
+        });
+    }
+    println!(
+        "client_get_one_errs.len(): {}",
+        client_get_one_errs.lock().unwrap().len()
+    );
+
+    if !serde_json_errs.lock().unwrap().is_empty() {
+        println!("serde_json errs: {:#?}", serde_json_errs);
+        time_it!(at once | "writing serde json errors to ron file" => {
+            let output_path = Path::new("temp/serde_json_errs.ron");
+            let output_array = serde_json_errs.clone().lock().unwrap().iter().map(|err| err.to_string()).collect::<Vec<_>>();
+
+            let mut file = File::create(output_path).unwrap();
+            let serde_json_errs_pretty = ron::ser::to_string_pretty(&output_array, Default::default()).unwrap();
+            file.write_all(serde_json_errs_pretty.as_bytes()).unwrap();
+        });
+    }
+    println!(
+        "serde_json_errs.len(): {}",
+        serde_json_errs.lock().unwrap().len()
     );
 
     println!("all_items.len(): {}", all_items.len());
@@ -95,5 +146,33 @@ fn main() {
         "number of cache misses; {}",
         cache.clone().read().unwrap().get_misses().len()
     );
+
+    let stats = cache.clone().read().unwrap().get_stats();
+
+    if stats.cache_hits + stats.cache_misses != stats.cache_size as u64 {
+        println!(
+            "cache hits ({}) + cache misses ({}) != cache size ({})",
+            stats.cache_hits, stats.cache_misses, stats.cache_size
+        );
+
+        let misses = cache.clone().read().unwrap().get_misses();
+        let hits = cache.clone().read().unwrap().get_hits();
+        let all = cache
+            .clone()
+            .read()
+            .unwrap()
+            .cache
+            .keys()
+            .cloned()
+            .collect::<Vec<_>>();
+
+        let missing = all
+            .iter()
+            .filter(|key| !misses.contains(key) && !hits.contains(key))
+            .collect::<Vec<_>>();
+
+        println!("missing: {:#?}", missing);
+    }
+
     println!("time taken: {:#?}", start.elapsed());
 }
