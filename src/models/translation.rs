9e50e2f7680f86ed5f99fd6bdd835a6f43834fef
pawarherschel <pawarherschel@gmail.com>
2023-10-05 23:50:03 +0530

    using my own translation errors derived from lingual's errors

--- src/models/translation.rs
+++ src/models/translation.rs
@@ -1,19 +1,38 @@
 use crate::zaphkiel::cache::Cache;
 use lingual::{blocking, Lang};
+use serde::{Deserialize, Serialize};
 use std::sync::{Arc, RwLock};
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum TranslationError {
+    TargetLangMismatch,
+    ParseIntErr,
+    HttpErr(String),
+    UrlParseErr,
+    JsonParseErr,
+}
+
+#[inline(always)]
 pub fn translate(
     text: impl AsRef<str>,
     to_lang: Lang,
     cache: Option<Arc<RwLock<Cache>>>,
-) -> Result<String, lingual::Errors> {
+) -> Result<String, TranslationError> {
     if let Some(cache) = &cache {
         if let Some(translation) = cache.read().unwrap().get(&text.as_ref().into()) {
             return Ok(translation);
         }
     }
     let text = text.as_ref();
-    let translation = blocking::translate(text, None, Some(to_lang))?;
+    let translation = blocking::translate(text, None, Some(to_lang)).map_err(|e| match e {
+        lingual::Errors::HttpErr(e) => TranslationError::HttpErr(e),
+        lingual::Errors::UrlParseErr => TranslationError::UrlParseErr,
+        lingual::Errors::JsonParseErr => TranslationError::JsonParseErr,
+        lingual::Errors::ParseIntErr => TranslationError::ParseIntErr,
+    })?;
+    if translation.target_lang() != to_lang {
+        return Err(TranslationError::TargetLangMismatch);
+    }
     if let Some(cache) = cache {
         cache
             .write()
